37018b77d7f45803aeeb55e00297a25981558083
Sagnik <sagnik.adusumilli@mail.utoronto.ca>
2019-01-31 20:46:23 -0500

    added code for basic processing

--- code.r
+++ code.r
@@ -1,4 +1,20 @@
 #TBD: Data extration from files
+#import packages;
+library(dplyr)
+library(reshape2)
+library(ggplot2)
+library(Hmisc)
+library(corrplot)
+library(mice)
+library(VIM)
+library(pROC)
+library(caret)
+library(sqldf)
+getwd();
+hospital=read.csv(file.choose(), header = TRUE, na.strings = c("NA","","#NA"))
+sevierty=read.csv(file.choose(), header = TRUE, na.strings = c("NA","","#NA"))
+head(hospital)
+str(hospital)
 
 #TBD: Data prep for different models (includes visuals)
 
